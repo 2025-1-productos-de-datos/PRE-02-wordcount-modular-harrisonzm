d3f823afbe795ca31d4ed38f6a7e48a0f85aa464
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-05-10 09:26:21 -0500

    fix imports

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,11 +1,11 @@
 # obtain a list of files in the input directory
 import sys
 
-from ._internals.count_words import count_words
-from ._internals.preprocess_lines import preprocess_lines
-from ._internals.read_all_lines import read_all_lines
-from ._internals.split_into_words import split_into_words
-from ._internals.write_word_counts import write_count_words
+from homework.src._internals.count_words import count_words
+from homework.src._internals.preprocess_lines import preprocess_lines
+from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_count_words
 
 
 def main():
